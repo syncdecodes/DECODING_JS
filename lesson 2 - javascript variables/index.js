897789eabb36de86dd4418f87154d489c2cca338
syncdecodes <wixved09@gmail.com>
2025-07-11 11:04:45 +0530

    Add files via upload

---
+++ lesson 2 - javascript variables/index.js
@@ -0,0 +1,15 @@
+console.log("lets learn ultimate javascript")
+
+let a = 5;
+console.log(a)
+a = "sia"
+console.log(a)
+
+var b = 5;
+console.log(b)
+b = "sakshi"
+console.log(b)
+
+// let or var (name) must begin with $ _ or a letter
+
+// javascript is case sensitive 
